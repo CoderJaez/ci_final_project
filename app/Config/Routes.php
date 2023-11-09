f109e780620a5abd4926dda1c0e1af546f4a2402
Rufino John <aguilarufino@gmail.com>
2023-11-09 16:45:19 +0800

    added rest api for author

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
 
-$routes->get('authors', 'AuthorController::index');
+
+$routes->resource('authors', ['controller' => 'AuthorController', 'except' => ['new', 'edit']]);
+
 
 service('auth')->routes($routes);
--- app/Controllers/AuthorController.php
+++ app/Controllers/AuthorController.php
@@ -2,12 +2,119 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-
-class AuthorController extends BaseController
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\Response;
+class AuthorController extends ResourceController
 {
+     /**
+     * Return an array of resource objects, themselves in array format
+     *
+     * @return mixed
+     */
     public function index()
     {
         //
+    }
+
+    /**
+     * Return the properties of a resource object
+     *
+     * @return mixed
+     */
+    public function show($id = null)
+    {
+        $author = new \App\Models\Author();
+        $data = $author->find($id);
+        return $this->response->setStatusCode(Response::HTTP_OK)->setJSON($data);
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters
+     *
+     * @return mixed
+     */
+    public function create()
+    {
+        $author = new \App\Models\Author();
+        $data = $this->request->getPost();
+
+        if (!$author->validate($data)){
+            $response = array(
+                'status' => 'error',
+                'error' => true,
+                'messages' => $author->errors()
+            );
+
+            return $this->response->setStatusCode(Response::HTTP_BAD_REQUEST)->setJSON($response);
+        }
+
+        $author->insert($data);
+        $response = array(
+            'status' => 'success',
+            'error' => false,
+            'messages' => 'Author added successfully'
+        );
+
+        return $this->response->setStatusCode(Response::HTTP_CREATED)->setJSON($response);
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties
+     *
+     * @return mixed
+     */
+    public function update($id = null)
+    {
+        $author = new \App\Models\Author();
+        $data = $this->request->getJSON();
+        unset($data->id);
+
+        if (!$author->validate($data)){
+            $response = array(
+                'status' => 'error',
+                'error' => true,
+                'messages' => $author->errors()
+            );
+
+            return $this->response->setStatusCode(Response::HTTP_NOT_MODIFIED)->setJSON($response);
+        }
+
+        $author->update($id, $data);
+        $response = array(
+            'status' => 'success',
+            'error' => false,
+            'messages' => 'Author updated successfully'
+        );
+
+        return $this->response->setStatusCode(Response::HTTP_OK)->setJSON($response);
+    }
+
+    /**
+     * Delete the designated resource object from the model
+     *
+     * @return mixed
+     */
+    public function delete($id = null)
+    {
+        $author = new \App\Models\Author();
+
+        if ($author->delete($id)){
+            $response = array(
+                'status' => 'success',
+                'error' => false,
+                'messages' => 'Author deleted successfully'
+            );
+
+            return $this->response->setStatusCode(Response::HTTP_OK)->setJSON($response);
+        }
+
+        $response = array(
+            'status' => 'error',
+            'error' => true,
+            'messages' => 'Author not found'
+        );
+
+        return $this->response->setStatusCode(Response::HTTP_NOT_FOUND)->setJSON($response);
+        
     }
 }
